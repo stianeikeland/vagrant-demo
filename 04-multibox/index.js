aa328531a0f0159730a7752692cfc28167a764b1
Stian Eikeland <stian@eikeland.se>
2014-10-21 16:00:38 +0200

    Multi VM example

---
+++ 04-multibox/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var redis = require('redis');
+
+var app = express();
+var db  = redis.createClient(6379, '192.168.33.3');
+
+db.set('visitors', 0);
+
+app.get('/', function (req, res) {
+    db.incr('visitors', function(err, visitors) {
+        res.send('<h1>Hallaien Bergen!</h1>' +
+                 '<h2>Besøkende: ' + visitors + '</h2>');
+    });
+});
+
+app.listen(4000, function () {
+    console.log('Hallaien kjører på http://localhost:4000/');
+});
